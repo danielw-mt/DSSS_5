e683b5733f00e6dea0fc4fab6f75d32cf5b6f629
daniel_wagner <daniel.wag@gmx.net>
2022-12-04 12:04:15 +0100

    Adapt directory structure according to exercise

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
-from setuptools import setup
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(
-    name='pysnowflake',
+    name='snowflake',
     version='0.1.0',    
     description='A python package that makes it snow',
     url='https://github.com/danielw-mt/DSSS_5',
     author='Daniel Wagner',
     author_email='dan.wagner@fau.de',
-    license='BSD 2-clause',
-    packages=['snowflake'],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+    packages=find_packages(),
 )
